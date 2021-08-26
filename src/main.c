b9e1db3d113fb92c946149ddd030a1fc9ff8cf82
kkocdko <kkocdko@163.com>
2021-08-26 16:58:54 +0800

    Reduce code

--- src/main.c
+++ src/main.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include <windows.h>
 
+BOOL WINAPI FakeGetComputerName(LPTSTR _0, LPDWORD _1) { return 0; }
+
+BOOL WINAPI FakeGetVolumeInformation(LPCTSTR _0, LPTSTR _1, DWORD _2,
+                                     LPDWORD _3, LPDWORD _4, LPDWORD _5,
+                                     LPTSTR _6, DWORD _7) {
+  return 0;
+}
+
 BOOL WINAPI FakeCryptData(DATA_BLOB *pIn, LPWSTR *_0, DATA_BLOB *_1, PVOID _2,
                           CRYPTPROTECT_PROMPTSTRUCT *_3, DWORD _4,
                           DATA_BLOB *pOut) {
@@ -12,17 +20,12 @@ BOOL WINAPI FakeCryptData(DATA_BLOB *pIn, LPWSTR *_0, DATA_BLOB *_1, PVOID _2,
   return TRUE;
 }
 
-BOOL WINAPI FakeGetComputerName(LPTSTR _0, LPDWORD _1) { return 0; }
+typedef int (*EntryFn)();
+EntryFn OriginEntry = NULL;
 
-BOOL WINAPI FakeGetVolumeInformation(LPCTSTR _0, LPTSTR _1, DWORD _2,
-                                     LPDWORD _3, LPDWORD _4, LPDWORD _5,
-                                     LPTSTR _6, DWORD _7) {
-  return 0;
-}
-
-void LoadHooks() {
+int Entry() {
   // Hook some API about encryption and protect
-  // `rlz/win/lib/machine_id_win.cc` and `components/os_crypt/os_crypt_win.cc`
+  // `rlz/win/lib/machine_id_win.cc`, `components/os_crypt/os_crypt_win.cc`
   HMODULE kernel32 = LoadLibrary("kernel32.dll");
   HMODULE crypt32 = LoadLibrary("crypt32.dll");
   MH_CreateHook((LPVOID)GetProcAddress(kernel32, "GetComputerNameW"),
@@ -34,32 +37,19 @@ void LoadHooks() {
   MH_CreateHook((LPVOID)GetProcAddress(crypt32, "CryptUnprotectData"),
                 (LPVOID)FakeCryptData, NULL);
   MH_EnableHook(MH_ALL_HOOKS);
-}
 
-typedef int (*EntryFn)();
-EntryFn OriginEntry = NULL;
+  const char *loadedFlag = "CRKNOB_LOADED";
+  if (GetEnvironmentVariable(loadedFlag, NULL, 0))
+    return OriginEntry();
+  SetEnvironmentVariable(loadedFlag, "");
 
-int Entry() {
-  LoadHooks();
-
-  const char *line = GetCommandLine(); // Example: ["C:\foo.exe"   --bar]
+  const char *line = GetCommandLine(); // Example: `"C:\foo.exe" --bar`
   const char *skipFirst = strchr(line + 1, line[0] == '"' ? '"' : ' ') + 1;
-  while (*skipFirst == ' ')
-    skipFirst += 1;
-
-  const char *mark1 = "--crknob-loaded ";
-  const char *mark2 = "--type="; // Current is sub process
-  if (strncmp(skipFirst, mark1, strlen(mark1)) == 0 ||
-      strncmp(skipFirst, mark2, strlen(mark2)) == 0)
-    return OriginEntry(); // Already loaded, return to origin entry
-
-  const char *insert = // Insert after argv[0], allow to overwrite from cmd
-      " --crknob-loaded"
-      " --force-local-ntp"
+  const char *insert = // Insert after argv[0], allow to overwrite again
       " --disable-features=RendererCodeIntegrity"
+      " --force-local-ntp"
       " --user-data-dir=\"User Data\"" // TODO: absolute
       " ";
-
   char args[32768] = {0}; // Max length, https://stackoverflow.com/a/28452546
   strncpy(args, line, skipFirst - line); // Keep argv[0]
   strcat(args, insert);
@@ -77,7 +67,6 @@ __declspec(dllexport) BOOL WINAPI
     DllMain(HINSTANCE _hinstDLL, DWORD fdwReason, LPVOID _lpReserved) {
   if (fdwReason != DLL_PROCESS_ATTACH)
     return TRUE;
-
   MODULEINFO info;
   GetModuleInformation(GetCurrentProcess(), GetModuleHandle(NULL), &info,
                        sizeof(MODULEINFO));
